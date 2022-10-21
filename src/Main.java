b973dd3521fe277ba65d63a9c2b81512e6000fa5
OlgaJav22 <olga.ko4erga@yandex.ru>
2022-10-22 00:57:53 +0300

    1 задача готова

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     //Задача 1
     public static int calculateYear(int leapYear) {
         if (leapYear % 4 == 0 && leapYear % 100 != 0 || leapYear % 400 == 0) {
-            System.out.println(leapYear + "- високосный год");
+            System.out.println(leapYear + "- високосный год.");
         } else {
-            System.out.println(leapYear + "- не високосный год");
+            System.out.println(leapYear + "- не високосный год.");
         }
         return leapYear;
     }
